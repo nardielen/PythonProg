a1504c0703fc20d7a3715a259ba4a72f7ef70cba
nardielen <127646672+nardielen@users.noreply.github.com>
2023-03-12 13:36:38 +0800

    Add files via upload

---
+++ item.py
@@ -0,0 +1,20 @@
+class Item:
+
+    code = ''
+    title = ''
+    description = ''
+    category = ''
+    quantityInStock = 0
+    price = 0
+
+    def viewFullDescription(self):
+        print()
+        return print
+    
+    def addToCart(self):
+        print()
+        return print
+    
+    def updateStockLevel(self):
+        print()
+        return print
