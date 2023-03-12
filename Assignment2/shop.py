9f79b8385ad63e17664ca92c8c89f7175eb6dc1a
nardielen <127646672+nardielen@users.noreply.github.com>
2023-03-12 14:27:06 +0800

    Update shop.py

---
+++ Assignment2/shop.py
@@ -0,0 +1,24 @@
+class Shop:
+
+    name = ''
+    description = ''
+    listOfCategories = ''
+    listOfItems = ''
+    url = ''
+
+    def viewCategory():
+        print()
+        return print
+    
+    def searchItems():
+        print()
+        return print
+    
+    def viewItems():
+        print()
+        return print
+    
+    def viewShoppingCart():
+        print()
+        return print
+    
